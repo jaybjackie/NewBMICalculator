2f434b065aa19898915b7533b6ab362f3e7f5aba
jaybjackie <kodchakan.forwork@gmail.com>
2023-03-30 10:38:41 +0700

    Initial Commit

---
+++ NewBMICalculator/NewBMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBMICalculatorApp.swift
+//  NewBMICalculator
+//
+//  Created by Jay B on 30/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct NewBMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
